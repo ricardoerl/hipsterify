1a1a184633e44c9d7dc7f48cf40917e55bdbe6ad
Ricardo Ramirez <hola@ricardoerl.com>
2015-03-31 18:59:28 -0600

    Almost functional whit problems on search

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,15 +12,32 @@ angular.module('hipsterifyApp')
     
 	    $scope.dirty = {};
 	    $scope.artist = {};
-
-		var states = ['Alabama', 'Alaska', 'California'];
+	    $scope.artistList = [];
+	    $scope.artistLevels = [
+	    	"Too mainstream to be here. The music was good but everything changed after collaborating with Lil Jon and Pitbull.",
+	    	"On the way to be the next PSY but still with some way to go. Start to pray that no one keep listening this.",
+	    	"Don't know whether to thank Spotify to hide this diamond or just nobody likes this.",
+	    	"Does this exist, anyone?"
+	    ];
 
 		function selectArtist(selected){
 			$scope.dirty = {};
-			$scope.artist.name = selected.value; 
+			$scope.artist = {};
+			$scope.artist = selected.obj;
+
+			if (selected.obj.popularity >= 0 && selected.obj.popularity <= 25) {
+				$scope.artist.level = $scope.artistLevels[3];
+			} else if (selected.obj.popularity > 25 && selected.obj.popularity <= 50) {
+				$scope.artist.level = $scope.artistLevels[2];
+			} else if (selected.obj.popularity > 50 && selected.obj.popularity <= 75) {
+				$scope.artist.level = $scope.artistLevels[1];
+			} else if (selected.obj.popularity > 75 && selected.obj.popularity <= 100) {
+				$scope.artist.level = $scope.artistLevels[0];
+			};
 		};
 
 		function deleteArtist(){
+			$scope.dirty = {};
 			$scope.artist = {};
 		};
 
@@ -31,28 +48,31 @@ angular.module('hipsterifyApp')
 			var q = term.toLowerCase().trim();
 			var results = [];
 
-		 //    Spotify.search(q, 'artist', { limit: 5 }).then(function (data) {
-			// 	for( var artist in data.artists.items ){
-			// 		results.push({ 
-			// 			value: data.artists.items[artist].name,
-			// 			label: data.artists.items[artist].name 
-			// 		});
-			// 	}
-			// });
+		    Spotify.search(q, 'artist', { limit: 5 }).then(function (data) {
+		    	$scope.artistList = [];
 
-			  for (var i = 0; i < states.length && results.length < 10; i++) {
-			      var state = states[i];
-			      if (state.toLowerCase().indexOf(q) === 0)
-			        results.push({ label: state, value: state });
-			    }
 
-			return results;
+				for( var artist in data.artists.items ){
+
+					$scope.artistList.push({ 
+						label: $sce.trustAsHtml(
+							'<div class="suggest-item">' + data.artists.items[artist].name + '</div>'
+						), 
+						value: data.artists.items[artist].name,
+						obj: data.artists.items[artist]
+					});
+
+				}
+				
+			});
+
+			return $scope.artistList;
 			
 		};
 
 		$scope.autocomplete_options = {
 		  suggest: suggestArtist,
-		  on_detach: deleteArtist,
+		  on_select: selectArtist,
 		  on_attach: deleteArtist
 		};
   });
